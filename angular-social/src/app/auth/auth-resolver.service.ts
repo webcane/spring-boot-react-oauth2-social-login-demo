10d011ddca798fdd6559733693e919d878e78df2
Mikhail Niedre <mikhail.niedre@t-systems.com>
2021-01-05 19:17:35 +0300

    redirect to /profile after login

--- angular-social/src/app/auth/auth-resolver.service.ts
+++ angular-social/src/app/auth/auth-resolver.service.ts
@@ -21,7 +21,7 @@ export class AuthResolver implements Resolve<boolean> {
 
       // not logged in so redirect to login page with the return url
       // this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
-      this.router.navigate(['/']);
+      this.router.navigate(['/profile']);
     }
 
     return this.authService.isAuthenticated();
